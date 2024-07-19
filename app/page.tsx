a8dfd96fd468d27c3243f5490ad97eb21d12e05a
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-07-19 10:33:29 +0700

    Create login page

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { ArrowRightIcon } from 'lucide-react'
 
 import AppLogo from '@/components/shared/app-logo'
 import { lusitana } from '@/components/shared/fonts'
+import { Button } from '@/components/ui/button'
 
 export default function Page() {
 	return (
@@ -11,6 +12,7 @@ export default function Page() {
 			<div className='flex h-20 shrink-0 items-center rounded-lg p-4 md:h-40 bg-secondary'>
 				<AppLogo />
 			</div>
+
 			<div className='mt-4 flex grow flex-col gap-4 md:flex-row'>
 				<div className='flex flex-col justify-center gap-6 rounded-lg  px-6 py-10 md:w-2/5 md:px-20'>
 					<p
@@ -18,11 +20,12 @@ export default function Page() {
 					>
 						<strong>Welcome to Next 15 Admin Dashboard.</strong>
 					</p>
-
+					<Button asChild className='w-1/2'></Button>
 					<Link href='/login'>
 						<span>Log in</span> <ArrowRightIcon className='w-6' />
 					</Link>
 				</div>
+
 				<div className='flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12'>
 					<Image
 						src='/hero-desktop.png'
---
+++ auth.ts
@@ -0,0 +1,29 @@
+import NextAuth from 'next-auth'
+import credentials from 'next-auth/providers/credentials'
+import { compare } from 'bcryptjs'
+
+import { authConfig } from './auth.config'
+import { users } from './lib/placeholder-data'
+
+export const { auth, signIn, signOut } = NextAuth({
+	...authConfig,
+	providers: [
+		credentials({
+			async authorize(credentials) {
+				const user = users.find((x) => x.email === credentials.email)
+
+				if (!user) return null
+
+				const passwordsMatch = await compare(
+					credentials.password as string,
+					user.password
+				)
+
+				if (passwordsMatch) return user
+
+				console.log('Invalid credentials')
+				return null
+			},
+		}),
+	],
+})
