7ceae735e19cd365ac00a5b8bd0986d0624ee638
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-07-21 16:27:01 +0700

    Authenticate user from database

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,8 +1,11 @@
 'use server'
 
 import { AuthError } from 'next-auth'
+import { eq } from 'drizzle-orm'
 
 import { signIn } from '@/auth'
+import { users } from '@/db/schema'
+import db from '@/db/drizzle'
 
 export async function authenticate(
 	prevState: string | undefined,
@@ -21,4 +24,13 @@ export async function authenticate(
 		}
 		throw error
 	}
+}
+
+export async function getUser(email: string) {
+	const user = await db.query.users.findFirst({
+		where: eq(users.email, email as string),
+	})
+
+	if (!user) throw new Error('User not found')
+	return user
 }
